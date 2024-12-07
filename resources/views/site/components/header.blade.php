2b6018651354733ed72501f86c5a2ff2d31326b6
August0ka <kiichi_sassagima@outlook.com>
2024-12-07 18:57:23 -0400

    ✨ Feat(app): add app file

--- resources/views/site/components/header.blade.php
+++ resources/views/site/components/header.blade.php
@@ -1,25 +1,34 @@
 <header>
-    <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+    <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile_menu">
+        {{-- <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0"> --}}
+        <ul class="absolute h-full bg-gray-700 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+            <li>
+                <a href="#"
+                    class="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0"
+                    aria-current="page">Home</a>
+            </li>
+            <li>
+                <a href="#"
+                    class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 
+                      lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0">Produto</a>
+            </li>
+            <li>
+                <a href="#"
+                    class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 
+                    lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700
+                    lg:p-0">Categoria</a>
+            </li>
+        </ul>
+    </div>
+    <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
             <a href="https://flowbite.com" class="flex items-center">
                 <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-                <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Lojinha legal</span>
+                <span class="self-center text-xl font-semibold whitespace-nowrap">Lojinha legal</span>
             </a>
-            <div class="flex items-center lg:order-2">
-                <a href="#"
-                    class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4
-                      focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 
-                      lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">
-                    Log in
-                </a>
-                <a href="#"
-                    class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4
-                     focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2
-                     lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">
-                    Cadastrar
-                </a>
-                <button data-collapse-toggle="mobile-menu-2" type="button"
-                    class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            <div class="flex items-center justify-between w-full lg:order-2">
+                <button data-collapse-toggle="mobile-menu-2" type="button" id="hamburger_button"
+                    class="items-center text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
                     aria-controls="mobile-menu-2" aria-expanded="false">
                     <span class="sr-only">Open main menu</span>
                     <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -34,29 +43,20 @@
                             clip-rule="evenodd"></path>
                     </svg>
                 </button>
-            </div>
-            <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
-                <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                    <li>
-                        <a href="#"
-                            class="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
-                            aria-current="page">Home</a>
-                    </li>
-                    <li>
-                        <a href="#"
-                            class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 
-                              lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700
-                              lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700
-                             dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Produto</a>
-                    </li>
-                    <li>
-                        <a href="#"
-                            class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 
-                            lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700
-                            lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700
-                            dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Categoria</a>
-                    </li>
-                </ul>
+                <div class="">
+                    <a href="#"
+                        class="text-gray-800 hover:bg-gray-50 focus:ring-4
+                          focus:ring-gray-300 font-medium rounded-lg text-sm lg:px-5 py-2 
+                          lg:py-2.5 focus:outline-none">
+                        Log in
+                    </a>
+                    <a href="#"
+                        class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4
+                         focus:ring-primary-300 font-medium rounded-lg text-sm lg:px-5 py-2
+                         lg:py-2.5 focus:outline-none">
+                        Cadastrar
+                    </a>
+                </div>
             </div>
         </div>
     </nav>
--- resources/views/site/layouts/app.blade.php
+++ resources/views/site/layouts/app.blade.php
@@ -1,20 +1,35 @@
 <!doctype html>
 <html>
+
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @vite('resources/css/app.css')
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
 </head>
+
 <body>
 
-<x-header />
-{{-- <x-header :categories="$categories" /> --}}
-@yield('site_content')
+    <x-header />
+    {{-- <x-header :categories="$categories" /> --}}
+    @yield('site_content')
 
 
-<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#hamburger_button').on('click', function() {
+                if ($('#mobile_menu').hasClass('hidden')) {
+                    $('#mobile_menu').removeClass('hidden');
+                } else {
+                    $('#mobile_menu').addClass('hidden');
+                }
+            });
+        });
+    </script>
 
-@yield('site_scripts')
+    @yield('site_scripts')
 </body>
+
 </html>
