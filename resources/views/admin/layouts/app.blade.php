f48d6a90bd9ecb128a587dc5a41317f3d817d1b0
August0ka <kiichi_sassagima@outlook.com>
2025-01-24 10:17:20 -0400

    ✨ Feat(products): add products screen in admin

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -14,58 +14,81 @@
 <body>
     <div class="flex min-h-screen">
         <!-- Sidebar -->
-        <nav id="sidebarMenu" class="bg-vivid-violet-950 w-64 p-4 shadow-md">
-            <div class="flex justify-center">
+        <nav id="sidebarMenu" class="bg-vivid-violet-950 w-48 p-4 shadow-md">
+            <div class="flex justify-start">
                 <img class="h-20" src="{{ asset('images/banner.svg') }}" alt="Ludokai_banner">
             </div>
-            <div class="flex flex-col h-full">
-                <ul class="mb-4 space-y-4">
-                    <li>
-                        <button id="btnSwitch"
-                            class="btn-theme-toggle w-full px-4 py-2 bg-gray-800 text-white dark:bg-gray-200 dark:text-black rounded shadow">
-                            <i id="themeIcon" class="bi bi-moon-stars"></i>
-                        </button>
-                    </li>
-                </ul>
+            <div class="flex flex-col mt-5">
                 <div class="flex-1">
-                    <ul class="space-y-4">
-                        <li>
-                            <a class="flex items-center space-x-2 text-gray-900 dark:text-gray-100"
-                                href="">
-                                <i class="bi bi-people"></i>
-                                <span>Usuários</span>
-                            </a>
+                    <ul class="space-y-3">
+                        <li class="">
+                            <div class="flex items-center border-l border-gray-400 hover:border-gray-200 text-pumpkin-500 hover:text-pumpkin-300">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="ml-1.5 size-5">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
+                                </svg>
+
+                                <a class="flex items-center space-x-2 text-gray-400 hover:text-gray-100 ml-2" href="">
+                                    <span>Usuários</span>
+                                </a>
+                            </div>
                         </li>
                         <li>
-                            <a class="flex items-center space-x-2 text-gray-900 dark:text-gray-100"
-                                href="">
-                                <i class="bi bi-shop"></i>
-                                <span>Produtos</span>
-                            </a>
+                            <div class="flex items-center border-l border-gray-400 hover:border-gray-200 text-pumpkin-500 hover:text-pumpkin-300">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="ml-1.5 size-5">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="m21 7.5-9-5.25L3 7.5m18 0-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9" />
+                                </svg>
+
+                                <a class="flex items-center space-x-2 text-gray-400 hover:text-gray-100 ml-2" href="">
+                                    <span>Produtos</span>
+                                </a>
+                            </div>
                         </li>
                         <li>
-                            <a class="flex items-center space-x-2 text-gray-900 dark:text-gray-100"
-                                href="">
-                                <i class="bi bi-tags"></i>
-                                <span>Categorias</span>
-                            </a>
+                            <div class="flex items-center border-l border-gray-400 hover:border-gray-200 text-pumpkin-500 hover:text-pumpkin-300">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="ml-1.5 size-5">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0ZM3.75 12h.007v.008H3.75V12Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm-.375 5.25h.007v.008H3.75v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+                                </svg>
+
+                                <a class="flex items-center space-x-2 text-gray-400 hover:text-gray-100 ml-2" href="">
+                                    <span>Categorias</span>
+                                </a>
+                            </div>
                         </li>
                         <li>
-                            <a class="flex items-center space-x-2 text-gray-900 dark:text-gray-100"
-                                href="">
-                                <i class="bi bi-cash-coin"></i>
-                                <span>Vendas</span>
-                            </a>
+                            <div class="flex items-center border-l border-gray-400 hover:border-gray-200 text-pumpkin-500 hover:text-pumpkin-300">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor" class="ml-1.5 size-5">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+                                </svg>
+
+                                <a class="flex items-center space-x-2 text-gray-400 hover:text-gray-100 ml-2" href="">
+                                    <span>Vendas</span>
+                                </a>
+                            </div>
                         </li>
                     </ul>
                 </div>
                 <ul class="mt-4 space-y-4">
                     <li>
-                        <a class="flex items-center space-x-2 text-gray-900 dark:text-gray-100"
-                            href="{{ route('admin.logout') }}">
-                            <i class="bi bi-box-arrow-left"></i>
-                            <span>Sair</span>
-                        </a>
+                        <div class="flex items-center border-l border-gray-400 hover:border-gray-200 text-pumpkin-500 hover:text-pumpkin-300">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke-width="1.5" stroke="currentColor" class="ml-1.5 size-5">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75" />
+                            </svg>
+
+                            <a class="flex items-center space-x-2 text-gray-400 hover:text-gray-100 ml-2"
+                                href="{{ route('admin.logout') }}">
+                                <span>Sair</span>
+                            </a>
+                        </div>
                     </li>
                 </ul>
             </div>
--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -1,12 +1,18 @@
 @extends('admin.layouts.app')
 
 @section('content')
-    <div class="container mx-auto">
+    <div>
         <div class="flex justify-between items-center mt-5">
-            <div class="text-xl font-bold text-pumpkin-200">Produtos</div>
-            <div class="mt-2 mb-2 flex justify-end">
-                <a href=""
-                    class="bg-pumpkin-500 hover:bg-pumpkin-600 text-white py-1 px-4 rounded-full shadow">Adicionar</a>
+            <div class="text-2xl font-bold text-pumpkin-200">Produtos</div>
+            <div class="my-2">
+                <button
+                    class="flex items-center bg-pumpkin-500 hover:bg-pumpkin-600 text-white py-1 px-4 rounded-full shadow">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="text-white size-4 mr-1">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                    </svg>
+                    Adicionar
+                </button>
             </div>
         </div>
 
@@ -34,21 +40,27 @@
                             <td class="px-4 py-2">{{ $product->name }}</td>
                             <td class="px-4 py-2">{{ 'R$ ' . number_format($product->price, 2, ',', '.') }}</td>
                             <td class="px-4 py-2">{{ $product->quantity }}</td>
-                            <td class="px-4 py-2">
-                                <a href=""
-                                    class="inline-flex items-center justify-center bg-blue-500 hover:bg-blue-600 text-white text-sm px-3 py-2 rounded shadow"
-                                    data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Editar produto">
-                                    <i class="bi bi-pencil-square"></i>
-                                </a>
+                            <td class="flex px-4 py-2">
+                                <button
+                                    class="flex items-center justify-center bg-indigo-600 hover:bg-indigo-500 rounded-full p-1 mr-1.5">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="text-pumpkin-100 size-5">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
+                                    </svg>
+                                </button>
 
                                 <button type="button"
-                                    class="inline-flex items-center justify-center bg-red-500 hover:bg-red-600 text-white text-sm px-3 py-2 rounded shadow delete-button"
-                                    data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Excluir produto"
-                                    data-button-modal="{{ $product->id }}">
-                                    <i class="bi bi-trash3"></i>
+                                    class="flex items-center justify-center bg-pink-700 hover:bg-pink-600 rounded-full p-1">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                        stroke-width="1.5" stroke="currentColor" class="text-pumpkin-100 size-5">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                    </svg>
+
                                 </button>
                             </td>
-                        </tr>   
+                        </tr>
                     @endforeach
                 </tbody>
             </table>
