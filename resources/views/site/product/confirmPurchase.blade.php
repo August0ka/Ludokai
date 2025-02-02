50f9ab6682053ff8dae4e1067ca64dad5a622e8d
miriarezende <goncalvesmiria19@gmail.com>
2025-02-02 17:25:57 -0400

    🐛 Fix(confirmPurchase): fix layout

--- resources/views/site/product/confirmPurchase.blade.php
+++ resources/views/site/product/confirmPurchase.blade.php
@@ -2,13 +2,13 @@
 
 @section('site_content')
     <div class="container mt-5 mx-auto flex justify-center items-center w-1/2">
-        <div class="card p-6 shadow-lg rounded-lg bg-white">
+        <div class="card p-6 shadow-lg rounded-lg bg-pumpkin-500">
             <div class="flex items-center justify-center mb-4">
-                <h3 class="text-lg font-medium mr-2">Agradecemos pela compra</h3>
+                <h3 class="text-lg font-medium mr-2 text-gray-100">Agradecemos pela compra</h3>
                 <i class="bi bi-check-circle-fill text-green-500"></i>
             </div>
             <div class="mt-4 flex justify-center">
-                <a class="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600" href="/">Ok</a>
+                <a class="bg-emerald-600 text-gray-100 px-4 py-2 rounded-full hover:bg-emerald-700" href="/">Ok</a>
             </div>
         </div>
     </div>
