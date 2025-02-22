27f345f8bb4338f9422124e0c453822327933650
miriarezende <goncalvesmiria19@gmail.com>
2025-02-22 15:07:34 -0400

    🐛 Fix(header): fix header and search in mobile

--- resources/views/site/layouts/app.blade.php
+++ resources/views/site/layouts/app.blade.php
@@ -29,8 +29,16 @@
 
             $('#search-mobile-button').on('click', function() {
                 $('#mobile-search-form').removeClass('hidden').addClass('block');
-                $('#mobile-search-header').removeClass('hidden').addClass('block');
+                $('#mobile-search-header').removeClass('hidden').addClass('flex');
                 $('#web-header').removeClass('block').addClass('hidden');
+                $('#search-overlay').removeClass('hidden');
+            });
+
+            $('#search-overlay').on('click', function() {
+                $('#mobile-search-form').removeClass('block').addClass('hidden');
+                $('#mobile-search-header').removeClass('flex').addClass('hidden');
+                $('#web-header').removeClass('hidden').addClass('block');
+                $('#search-overlay').addClass('hidden');
             });
         });
     </script>
