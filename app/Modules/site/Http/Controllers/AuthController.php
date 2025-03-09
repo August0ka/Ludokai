4b5504801fc85cedae922e46318188579f9ccf2d
August0ka <kiichi_sassagima@outlook.com>
2025-03-09 13:50:47 -0400

    🐛 Fix(redirect): fix wrong redirect in site login

--- app/Modules/site/Http/Controllers/AuthController.php
+++ app/Modules/site/Http/Controllers/AuthController.php
@@ -31,7 +31,7 @@ class AuthController extends Controller
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
 
-            return redirect()->intended();
+            return redirect()->route('site.home');
         }
 
         return back()->withErrors([
