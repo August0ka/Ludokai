bc70ad25ff56a9680bd3f6dc9ce279cd0c4d6fcf
August0ka <kiichi_sassagima@outlook.com>
2025-03-09 12:08:25 -0400

    ⚡️ Feat(lang): add pt_BR language support

--- resources/views/site/register/index.blade.php
+++ resources/views/site/register/index.blade.php
@@ -44,21 +44,21 @@
                         </div>
                         <div class="col-span-3 md:col-span-3">
                             <label for="password" class="block text-pumpkin-300 font-medium mb-1">Senha</label>
-                            <input id="password" type="password"
+                            <input id="password" type="current-password"
                                 class="w-full bg-pumpkin-200 border border-gray-300 rounded-lg px-2 py-1 focus:outline-none focus:ring-2 focus:ring-pumpkin-500"
                                 name="password" required>
-                        </div>
-                        <div class="col-span-8 md:col-span-8">
-                            <label for="address" class="block text-pumpkin-300 font-medium mb-1">Endereço</label>
-                            <input id="address" type="text"
-                                class="w-full bg-pumpkin-200 border border-gray-300 rounded-lg px-2 py-1 focus:outline-none focus:ring-2 focus:ring-pumpkin-500"
-                                name="address" required>
                         </div>
                         <div class="col-span-4 md:col-span-4">
                             <label for="cep" class="block text-pumpkin-300 font-medium mb-1">CEP</label>
                             <input id="cep" type="text"
                                 class="w-full bg-pumpkin-200 border border-gray-300 rounded-lg px-2 py-1 focus:outline-none focus:ring-2 focus:ring-pumpkin-500"
                                 name="cep" required>
+                        </div>
+                        <div class="col-span-8 md:col-span-8">
+                            <label for="address" class="block text-pumpkin-300 font-medium mb-1">Endereço</label>
+                            <input id="address" type="text"
+                                class="w-full bg-pumpkin-200 border border-gray-300 rounded-lg px-2 py-1 focus:outline-none focus:ring-2 focus:ring-pumpkin-500"
+                                name="address" required>
                         </div>
                         <div class="col-span-6">
                             <label for="state" class="block text-pumpkin-300 font-medium mb-1">Estado</label>
@@ -93,36 +93,98 @@
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"
         integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
     <script>
         $(document).ready(function() {
+            showError();
+            
             $('#cpf').mask('000.000.000-00');
             $('#phone').mask('(00) 00000-0000');
             $('#cep').mask('00000-000');
 
             $('#cep').on('blur', function() {
                 let cep = $(this).val().replace('-', '');
-                console.log(cep);
-                $.ajax({
-                    type: "GET",
-                    url: `https://viacep.com.br/ws/${cep}/json/`,
-                    success: function(response) {
-                        if (response.erro) {
-                            console.log('CEP não encontrado');
-                            return;
-                        }
-                        $('#address').val(response.logradouro);
-                    }
-                });
+
+                setAddressByCEP(cep);
             });
 
             $('#city').on('click', function() {
                 let stateId = $('#state').val();
+
+                if (!stateId) {
+                    Swal.fire({
+                        title: "Atenção",
+                        text: "Selecione um estado antes de escolher a cidade.",
+                        icon: "warning",
+                    });
+                    return;
+                }
             })
 
             $('#state').change(function() {
                 let stateId = $(this).val();
 
+                setCitiesByState(stateId);
+            });
+        })
+
+        async function setAddressByCEP(cep) {
+            if (!cep) {
+                return;
+            }
+
+            try {
+                const cepResponse = await $.ajax({
+                    type: "GET",
+                    url: `https://viacep.com.br/ws/${cep}/json/`
+                });
+
+                if (cepResponse.erro) {
+                    Swal.fire({
+                        title: "Atenção",
+                        text: "CEP não encontrado",
+                        icon: "warning",
+                    });
+                    $('#address').val('');
+                    return;
+                }
+
+                $('#address').val(`${cepResponse.logradouro}, ${cepResponse.bairro}`);
+                const stateUF = cepResponse.uf;
+                const cityName = cepResponse.localidade;
+
+                const statesResponse = await $.ajax({
+                    type: "GET",
+                    url: `https://servicodados.ibge.gov.br/api/v1/localidades/estados`
+                });
+
+                const state = statesResponse.find((state) => stateUF === state.sigla);
+                if (!state) {
+                    return;
+                }
+                $('#state').val(state.id);
+
+                const citiesResponse = await $.ajax({
+                    type: "GET",
+                    url: `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${state.id}/municipios`
+                });
+
+                const city = citiesResponse.find((city) => cityName === city.nome);
+                if (!city) {
+                    return;
+                }
+
+                await setCitiesByState(state.id);
+                $('#city').val(city.id);
+
+            } catch (error) {
+                console.error("Erro ao processar o CEP:", error);
+            }
+        }
+
+        async function setCitiesByState(stateId) {
+            return new Promise((resolve, reject) => {
                 $.ajax({
                     type: "GET",
                     url: `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${stateId}/municipios`,
@@ -131,12 +193,29 @@
                         $('#city').append('<option value="">Selecione...</option>');
                         response.forEach(city => {
                             $('#city').append(
-                                `<option value="${city.id}">${city.nome}</option>`);
+                                `<option value="${city.id}">${city.nome}</option>`
+                            );
                         });
+                        resolve();
+                    },
+                    error: function(error) {
+                        reject(error);
                     }
                 });
             });
-        })
+        }
+
+        function showError() {
+            const sessionError = "{{ session('error') ? session('error') : '' }}";
+
+            if (sessionError) {
+                Swal.fire({
+                    title: "Atenção",
+                    text: sessionError,
+                    icon: "error",
+                });
+            }
+        }
     </script>
 </body>
 
