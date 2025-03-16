5f645c3d2c8b8999ee6b11187e1cbacbddbbc241
August0ka <kiichi_sassagima@outlook.com>
2025-03-16 12:35:17 -0400

    🐛 Fix(userCpf): add verification to validate cpf

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,9 +1,35 @@
 <?php
 
-function removeAccents($str) {
+function removeAccents($str)
+{
     if (class_exists('Normalizer')) {
         $str = Normalizer::normalize($str, Normalizer::FORM_D);
     }
 
     return preg_replace('/[\x{0300}-\x{036f}]/u', '', $str);
+}
+
+function validateCPF($cpf): bool
+{
+    $cpf = preg_replace('/[^0-9]/', '', $cpf);
+
+    if (strlen($cpf) != 11) {
+        return false;
+    }
+
+    if (preg_match('/^(\d)\1+$/', $cpf)) {
+        return false;
+    }
+
+    for ($t = 9; $t < 11; $t++) {
+        for ($d = 0, $c = 0; $c < $t; $c++) {
+            $d += $cpf[$c] * (($t + 1) - $c);
+        }
+        $d = ((10 * $d) % 11) % 10;
+        if ($cpf[$c] != $d) {
+            return false;
+        }
+    }
+
+    return true;
 }
